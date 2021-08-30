07bccb40d0cf292f469be54d826b67075a7f10a8
ryanpiv <ryanpiveteau@gmail.com>
2021-08-29 21:21:50 -0400

    typo aws provider

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => {
           accessKeyId: env('AWS_ACCESS_KEY_ID'),
           secretAccessKey: env('AWS_ACCESS_SECRET'),
           region: env('AWS_REGION'),
-          parms: {
+          params: {
             Bucket: env('AWS_BUCKET')
           }
         }
